87914e55de5c8921d2b821e77cd09f7f2887089f
Onoyama Rikuo <onoyama_e_rikuo@matondo.net>
2020-04-23 14:12:36 +0900

    apiルートの修正

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::get('php','YoutubeController@getPHP');
-Route::get('java','YoutubeController@getJava');
-Route::get('ruby','YoutubeController@getRuby');
-Route::get('python','YoutubeController@getPython');
-Route::get('javascript','YoutubeController@getJavaScript');
-Route::get('go','YoutubeController@getGo');
+Route::get('api/php','YoutubeController@getPHP');
+Route::get('api/java','YoutubeController@getJava');
+Route::get('api/ruby','YoutubeController@getRuby');
+Route::get('api/python','YoutubeController@getPython');
+Route::get('api/javascript','YoutubeController@getJavaScript');
+Route::get('api/go','YoutubeController@getGo');
 
